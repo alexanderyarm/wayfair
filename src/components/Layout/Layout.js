f853dbda2fc2e96ba625c6265d4bc422c78fa5c9
yarm <yarm.alexander@gmail.com>
2021-01-23 17:21:28 +0100

    Layout component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import { Content } from "../Content/Content";
+
+const StyledLayout = styled.div`
+  display: grid;
+  grid-template-areas:
+    "nav header"
+    "nav content";
+  grid-template-rows: auto 1fr;
+  grid-template-columns: auto 1fr;
+  min-height: 100vh;
+`;
+
+const StyledNavigation = styled.nav`
+  grid-area: nav;
+  border-right: 1px solid #ccc;
+  height: 100%;
+`;
+
+const StyledHeader = styled.header`
+  background: #fff;
+  grid-area: header;
+  position: sticky;
+  top: 0;
+`;
+
+const StyledContent = styled.main`
+  grid-area: content;
+  height: 2000px;
+`;
+
+export const Layout = () => {
+  return (
+    <StyledLayout>
+      <StyledHeader>header</StyledHeader>
+
+      <StyledNavigation>Navigation</StyledNavigation>
+
+      <StyledContent>
+        <Content />
+      </StyledContent>
+    </StyledLayout>
+  );
+};
